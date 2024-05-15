3fef2dfe9c26fff1e16c8d566facad698632e467
Hartz-byte <67004713+Hartz-byte@users.noreply.github.com>
2024-05-15 14:14:33 +0530

    highlights update

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -1,9 +1,55 @@
-import React from 'react'
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import React from "react";
+import { rightImg, watchImg } from "../utils";
 
 const Highlights = () => {
-  return (
-    <div>Highlights</div>
-  )
-}
+  useGSAP(() => {
+    // title
+    gsap.to("#title", {
+      opacity: 1,
+      y: 0,
+    });
 
-export default Highlights
+    // watch film
+    gsap.to(".link", {
+      opacity: 1,
+      y: 0,
+      duration: 1,
+      stagger: 0.25,
+    });
+  });
+
+  return (
+    <section
+      id="highlights"
+      className="w-screen overflow-hidden h-full common-padding bg-zinc"
+    >
+      <div className="screen-max-width">
+        <div className="mb-12 w-full items-end md:flex justify-between">
+          {/* title */}
+          <h1 id="title" className="section-heading">
+            Get the highlights.
+          </h1>
+
+          {/* links */}
+          <div className="flex flex-wrap items-end gap-5">
+            {/* watch film */}
+            <p className="link">
+              Watch the film
+              <img src={watchImg} alt="watch" className="ml-2" />
+            </p>
+
+            {/* watch event */}
+            <p className="link">
+              Watch the event
+              <img src={rightImg} alt="right" className="ml-2" />
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Highlights;
